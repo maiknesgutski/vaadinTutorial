30c4f0098362b5b6041c88bdfe12553f5f65bae6
maikschell <maik.schell@ecclesia-gruppe.de>
2024-07-18 08:40:36 +0200

    added all the files

---
+++ src/main/java/com/example/application/views/channel/ChannelView.java
@@ -0,0 +1,88 @@
+package com.example.application.views.channel;
+
+import com.example.application.chat.ChatService;
+import com.example.application.chat.Message;
+import com.example.application.views.lobby.LobbyView;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.messages.MessageInput;
+import com.vaadin.flow.component.messages.MessageList;
+import com.vaadin.flow.component.messages.MessageListItem;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.Disposable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Route(value = "channel")
+public class ChannelView extends VerticalLayout implements HasUrlParameter<String> {
+
+    @Autowired
+    private ChatService chatService;
+    private MessageList messageList;
+    private String channelId;
+    private final List<Message> receivedMessages = new ArrayList<>();
+
+    public ChannelView(ChatService chatService) {
+        this.chatService = chatService;
+        setSizeFull();
+
+        this.messageList = new MessageList();
+        this.messageList.setSizeFull();
+        add(messageList);
+
+        var messageInput = new MessageInput(submitEvent -> sendMessage(submitEvent.getValue()));
+        messageInput.setWidthFull();
+        add(messageInput);
+    }
+
+    @Override
+    public void setParameter(BeforeEvent event, String channelId) {
+        if(chatService.channel(channelId).isEmpty()) {
+            event.forwardTo(LobbyView.class);
+        } else {
+            this.channelId = channelId;
+        }
+    }
+
+
+    private void sendMessage(String message) {
+        if (!message.isBlank()) {
+            chatService.postMessage(channelId, message);
+        }
+    }
+
+    private MessageListItem createMessageListItem(Message message) {
+        MessageListItem item = new MessageListItem(
+                message.message(),
+                message.timestamp(),
+                message.author()
+        );
+
+        return item;
+    }
+
+    private void receiveMessage(List<Message> incomingMessages) {
+        getUI().ifPresent(ui -> ui.access(() -> {
+            receivedMessages.addAll(incomingMessages);
+            messageList.setItems(receivedMessages
+                    .stream()
+                    // syntax sugar (method reference) the same as map(message -> this.createMessageListItem(message)
+                    .map(this::createMessageListItem)
+                    .toList());
+        }));
+    }
+
+    private Disposable subscribe() {
+        return chatService.liveMessages(channelId)
+                .subscribe(this::receiveMessage);
+    }
+
+    protected void onAttach(AttachEvent attachEvent){
+        var subscription = subscribe();
+        addDetachListener(detachEvent -> subscription.dispose());
+    }
+}
---
+++ src/main/java/com/example/application/views/lobby/LobbyView.java
@@ -0,0 +1,73 @@
+package com.example.application.views.lobby;
+
+import com.example.application.chat.Channel;
+import com.example.application.chat.ChatService;
+import com.example.application.views.channel.ChannelView;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.virtuallist.VirtualList;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+@Route(value = "")
+@PageTitle("Lobby")
+public class LobbyView extends VerticalLayout {
+
+    private final ChatService chatService;
+    private final VirtualList<Channel> channels;
+    private final TextField channelNameField;
+    private final Button addChannelButton;
+
+    public LobbyView(ChatService chatService) {
+        this.chatService = chatService;
+        setSizeFull();
+
+        channels = new VirtualList<>();
+        channels.setRenderer(new ComponentRenderer<>(this::createChannelComponent));
+        add(channels);
+        expand(channels);
+
+        channelNameField = new TextField();
+        channelNameField.setPlaceholder("New channel name");
+
+        addChannelButton = new Button("Add channel", buttonClickEvent -> addChannel());
+        addChannelButton.setDisableOnClick(true);
+
+        var toolbar = new HorizontalLayout(channelNameField, addChannelButton);
+        toolbar.setWidthFull();
+        toolbar.expand(channelNameField);
+        add(toolbar);
+    }
+
+    private void refreshChannels() {
+        channels.setItems(chatService.channels());
+    }
+
+    private void addChannel() {
+        try {
+            var nameOfNewChannel = channelNameField.getValue();
+            if (!nameOfNewChannel.isBlank()) {
+                chatService.createChannel(nameOfNewChannel);
+                channelNameField.clear();
+                refreshChannels();
+            }
+        } finally {
+            addChannelButton.setEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        refreshChannels();
+    }
+
+    private Component createChannelComponent(Channel channel) {
+        return new RouterLink(channel.name(), ChannelView.class, channel.id());
+    }
+}
